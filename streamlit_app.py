d761fbdcf3dc8b5ab7fcdf60a5801d3357809bce
akshithchappidi <60094672+akshithchappidi@users.noreply.github.com>
2023-09-15 16:28:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,28 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
+# Create a sidebar with links to the different sections of the portal.
+sidebar = st.sidebar()
+sidebar.title("Placement Portal")
+sidebar.link("Student Section")
+sidebar.link("Placement Section")
+sidebar.link("Company Section")
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+# Create a main content area.
+main_content = st.container()
+main_content.title("Student Section")
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+# Add a form for students to register for placement.
+registration_form = main_content.form("Registration Form")
+registration_form.text("Name")
+registration_form.text("Email")
+registration_form.text("Phone Number")
+registration_form.text("Resume")
+registration_form.submit()
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+# Add a list of job openings.
+job_openings = main_content.container()
+job_openings.title("Job Openings")
+for job_opening in job_openings:
+    job_openings.text(job_opening["title"])
+    job_openings.text(job_opening["description"])
+    job_openings.link("Apply Now") 
